222fe30ef7169bee3b87321a27f452c559f7895a
Benjamin Staffin <benley@gmail.com>
2013-05-14 03:21:40 -0700

    Monday night checkpoint.

--- buildy/butcher.py
+++ buildy/butcher.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python2.7
-""" HI IT'S VINCE WITH SLAPCHOP """
-#os.environ.update({'GIT_PYTHON_TRACE': 'full'})
-
+# Copyright 2013 Cloudscaling Inc. All Rights Reserved.
+#
 # This is terrible and incomplete. Don't judge me :-P
 
+"""Butcher: a distributed build system."""
+
+__author__ = 'Benjamin Staffin <ben@cloudscaling.com>'
+
+# If you want this, it has to happen before importing gitrepo:
+#os.environ.update({'GIT_PYTHON_TRACE': 'full'})
 
 import json
 import networkx
@@ -48,10 +53,19 @@ class Butcher(object):
 class Target(dict):
   """A build target."""
 
+  # The components that make up a build target:
+  params = ('repo', 'git_ref', 'path', 'target')
+
   def __init__(self, *args, **kwargs):
     dict.__init__(self, {'repo': None, 'git_ref': '',
                          'path': '', 'target': 'all'})
     self.update(*args, **kwargs)
+
+  def __hash__(self):
+    return hash(self.__repr__())
+
+  def __eq__(self, other):
+    return repr(self) == repr(other)
 
   def update(self, iterable=None, **kwargs):
     if iterable:
@@ -60,24 +74,44 @@ class Target(dict):
           for k in iterable:
             self[k] = iterable[k]
         else:
-          for (k, v) in iterable: self[k] = v
+          for (k, val) in iterable:
+            self[k] = val
       else:
         self.update(self.__parse_target(iterable))
     for k in kwargs:
       self[k] = kwargs[k]
 
-  # TODO: __getitem__ and __setitem__ and maybe __repr__
-  # See http://stackoverflow.com/a/2390997
+  def __setitem__(self, key, value):
+    if key not in ('repo', 'git_ref', 'path', 'target'):
+      raise ButcherError('Invalid parameter: %s' % key)
+    # TODO: value validation
+    dict.__setitem__(self, key, value)
 
-  def __str__(self):
-    if None in self.values():
-      # TODO: does this make sense?
-      return 'INCOMPLETE'
+  def __repr__(self):
+    if self.repo is None or self.target is None:
+      raise ButcherError('No valid repr for this incomplete target.')
+
     ret = ['//%(repo)s' % dict(self)]
     if self['path']:
       ret.append('/%s' % self['path'])
     ret.append(':%(target)s' % self)
     return ''.join(ret)
+
+  @property
+  def repo(self):
+    return self['repo']
+
+  @property
+  def git_ref(self):
+    return self['git_ref']
+
+  @property
+  def path(self):
+    return self['path']
+
+  @property
+  def target(self):
+    return self['target']
 
   @staticmethod
   def __parse_target(targetstr, current_repo=None):
@@ -112,6 +146,8 @@ class Target(dict):
         groups['git_ref'] = 'develop'
       if not groups['target']:
         groups['target'] = 'all'
+      if not groups['path']:
+        groups['path'] = ''
     except AttributeError:
       raise ButcherError('"%s" is not a valid build target.')
     #log.debug('parse_target: %s -> %s', targetstr, groups)
@@ -147,7 +183,8 @@ def resolve(args):
   if not args:
     log.error('Argument required.')
     app.quit(1)
-  print resolve_target(args[0])
+  print(Target(args[0]))
+
 
 class RepoState(object):
   """Hold git repo state."""
@@ -166,46 +203,47 @@ class RepoState(object):
 @app.command
 def build(args):
   """Build a target and its dependencies."""
-  rstate = RepoState()
 
   if not args:
-    log.error('Argument required.')
+    log.error('Target required.')
     app.quit(1)
-  target = args[0]
-  rtarget = resolve_target(target)
-  if rtarget != target:
-    log.info('Resolved target to: %s', rtarget)
 
+  target = Target(args[0])
+  log.info('Resolved target to: %s', target)
+
+  rstate = RepoState()
   pins = {}
   for pin in app.get_options().pinned_repos:
     ppin = Target(pin)
-    pins[ppin['repo']] = ppin['git_ref']
+    pins[ppin.repo] = ppin.git_ref
 
-  print "PINS: %s" % pins
+  log.debug('Pinned refs: %s', pins)
 
-  log.info('Building target: %s' % rtarget)
-  params = Target(target)
-  #params = parse_target(target)
-  #log.debug('Params: %s', params)
+  log.info('Building target: %s' % target)
 
-  if not already_built(params['repo'], params):
-    log.info('####### Loading repo: %s', params['repo'])
+  if not already_built(target):
+    log.info('####### Loading repo: %s', target.repo)
     try:
-      repo = rstate.GetRepo(params['repo'], params['git_ref'])
+      repo = rstate.GetRepo(target.repo, target.git_ref)
     except gitrepo.GitError as err:
-      log.fatal('Error while fetching //%s:', params['repo'])
+      log.fatal('Error while fetching //%s:', target.repo)
       log.fatal(err)
       app.quit(1)
 
-    builddata = load_buildfile(repo, params)
+    builddata = load_buildfile(repo, target.path)
 
     repos_loaded = set()
     repo_queue = set()
+    build_queue = set()
 
-    (subgraph, nextrepos) = parse(builddata, repo.name, params['path'])
+    (subgraph, nextrepos) = parse(builddata, repo.name, target.path)
     repos_loaded.add(repo.name)
     repo_queue.update(nextrepos)
     repo_queue.difference_update(repos_loaded)
+    if target.target == 'all':
+      for tgt in subgraph.nodes():
+        if tgt.repo == repo.name:
+          build_queue.add(tgt)
 
     while repo_queue:
       log.debug('Repos loaded: %s', ', '.join(repos_loaded))
@@ -228,14 +266,15 @@ def build(args):
 
     log.debug('Repos loaded: %s', ', '.join(repos_loaded))
     log.debug('Repo queue: %s', ', '.join(repo_queue))
+    log.debug('Build queue: %s', ', '.join([str(x) for x in build_queue]))
 
     print('REPO STATE:')
     print rstate.HeadList()
 
     print('graph:')
-    print subgraph.node
+    pprint.pprint(subgraph.node)
     print('edges:')
-    print subgraph.edge
+    pprint.pprint(subgraph.edge)
 
     # Load dependency graph by recursively parsing BUILD files
     # Decorate nodes with buildcache status
@@ -259,53 +298,44 @@ def parse(builddata, reponame, path=None):
   Returns:
     (DiGraph of this BUILD file, list of repos it refers to)
   """
-
   subgraph = networkx.DiGraph()
   nextrepos = set()
   #subgraph.add_node(reponame, {'repo': repo})
   if 'targets' in builddata:
-    for target in builddata['targets']:
-      targetname = resolve_target(target.pop('name'), reponame, path)
+    for tdata in builddata['targets']:
+      target = Target(target=tdata.pop('name'), repo=reponame, path=path)
       #t_node = nodes.node(targetname)
       #subgraph.add_node(targetname, obj=t_node)
-      subgraph.add_node(targetname, {'build_data': target})
-      if 'dependencies' in target:
-        for dep in target['dependencies']:
-          #TODO(ben): fully resolve dep name
-          dep = resolve_target(dep, current_repo=reponame)
+      subgraph.add_node(target, {'build_data': tdata})
+      if 'dependencies' in tdata:
+        for dep in tdata['dependencies']:
           dep_parsed = Target(dep)
-          if dep_parsed['repo'] is None:
+          if not dep_parsed.repo:
             dep_parsed['repo'] = reponame
-          if 'repo' not in dep_parsed:
-            dep_parsed['repo'] = reponame
-          else:
-            nextrepos.add(dep_parsed['repo'])
+          if not dep_parsed.path:
+            dep_parsed['path'] = path
+          nextrepos.add(dep_parsed.repo)
           #d_node = nodes.node(dep)
-          if dep not in subgraph.nodes():
+          if dep_parsed not in subgraph.nodes():
             #subgraph.add_node(dep, obj=d_node)
-            subgraph.add_node(dep, {'build_data': target})
-          subgraph.add_edge(targetname, dep)
+            subgraph.add_node(dep_parsed, {'build_data': {}})
+          subgraph.add_edge(target, dep_parsed)
       #subgraph.add_edge(reponame, targetname)
   return (subgraph, nextrepos)
 
 
-def load_buildfile(repo, params=None):
-  if params:
-    prefix = params['path'] or ''
-  else:
-    prefix = ''
-  filepath = os.path.join(prefix, 'OCS_BUILD.data')
-  log.debug('Repo %s: loading buildfile: %s', repo.name, filepath)
+def load_buildfile(repo, path=''):
+  filepath = os.path.join(path, 'OCS_BUILD.data')
+  log.debug('Repo %s: loading buildfile: /%s', repo.name, filepath)
   blob = repo.repo.head.commit.tree/filepath
   data = blob.data_stream.read()
   return json.loads(data)
 
 
-def already_built(repo, params):
+def already_built(target):
   """Stub. Always returns False."""
   # FIXME: implement or obviate.
-  _ = repo
-  _ = params
+  _ = target.repo
   return False
 
 
--- buildy/gitrepo.py
+++ buildy/gitrepo.py
@@ -1,3 +1,10 @@
+# Copyright 2013 Cloudscaling Inc. All Rights Reserved.
+
+"""Git repository wrapper module."""
+
+# If you want this, it has to happen before importing git, as silly as that is:
+#os.environ.update({'GIT_PYTHON_TRACE': 'full'})
+
 import git
 import gitdb
 import os
@@ -25,28 +32,28 @@ class GitRepo(object):
   def __init__(self, name, ref='develop'):
     opts = app.get_options()
     self.repo_baseurl = opts.repo_baseurl or self.defaults['repo_baseurl']
-    log.debug('Base url: %s', self.repo_baseurl)
+    #log.debug('Base url: %s', self.repo_baseurl)
 
     self.repo_basedir = opts.repo_basedir or self.defaults['repo_basedir']
-    log.debug('Base directory: %s', self.repo_basedir)
+    #log.debug('Base directory: %s', self.repo_basedir)
 
     self.name = name
-    log.debug('Repo name: %s', self.name)
+    #log.debug('Repo name: %s', self.name)
 
     self.origin_url = '%s/%s' % (self.repo_baseurl, self.name)
-    log.debug('Trusted origin URL: %s', self.origin_url)
+    log.debug('[%s] Fetching from: %s', self.name, self.origin_url)
 
     self.repodir = os.path.join(self.repo_basedir, self.name)
-    log.debug('Repo dir: %s', self.repodir)
+    log.debug('[%s] Repo dir: %s', self.name, self.repodir)
 
     try:
       self.repo = git.Repo(self.repodir)
     except git.exc.NoSuchPathError:
       self.repo = git.Repo.init(self.repodir, bare=False)
-      log.info('Repo initialized.')
+      log.debug('[%s] Repo initialized.', self.name)
     except git.exc.InvalidGitRepositoryError:
-      log.error("%s exists, but is not a valid git repo. Can't continue.",
-                self.repodir)
+      log.error("[%s] %s exists, but is not a valid git repo. Can't continue.",
+                self.name, self.repodir)
       raise GitError
 
     self.setorigin()
@@ -59,12 +66,13 @@ class GitRepo(object):
     try:
       origin = self.repo.remotes.origin
       if origin.url != self.origin_url:
-        log.debug('Changing origin url. Old: %s New: %s', origin.url,
-                  self.origin_url)
+        log.debug('[%s] Changing origin url. Old: %s New: %s',
+                  self.name, origin.url, self.origin_url)
         origin.config_writer.set('url', self.origin_url)
     except AttributeError:
       origin = self.repo.create_remote('origin', self.origin_url)
-      log.debug('Created remote "origin" with URL: %s', origin.url)
+      log.debug('[%s] Created remote "origin" with URL: %s',
+                self.name, origin.url)
 
   def fetchall(self):
     """Fetch all refs from the upstream repo."""
@@ -75,21 +83,21 @@ class GitRepo(object):
 
   def fetchref(self, ref):
     """Fetch a particular git ref."""
-    log.debug('Fetching ref: %s', ref)
+    log.debug('[%s] Fetching ref: %s', self.name, ref)
     fetch_info = self.repo.remotes.origin.fetch(ref).pop()
     return fetch_info.ref
 
   def sethead(self, ref):
     """Set head to a git ref."""
-    log.debug('Setting to ref %s', ref)
+    log.debug('[%s] Setting to ref %s', self.name, ref)
     try:
       ref = self.repo.rev_parse(ref)
     except gitdb.exc.BadObject:
       # Probably means we don't have it yet.
       ref = self.fetchref(ref)
-    log.debug('Setting head to %s', ref)
+    log.debug('[%s] Setting head to %s', self.name, ref)
     self.repo.head.reset(ref, working_tree=True)
-    log.debug('Head object: %s', self.currenthead)
+    log.debug('[%s] Head object: %s', self.name, self.currenthead)
 
   @property
   def currenthead(self):
