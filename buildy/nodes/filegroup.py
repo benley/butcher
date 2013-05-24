3f9ebd4a40a26497ea1dba07cfe28455fe74cba1
Benjamin Staffin <benley@gmail.com>
2013-05-23 21:04:30 -0700

    Kill off the dead nodes module

--- buildy/nodes/filegroup.py
+++
@@ -1,4 +0,0 @@
-from .generic import GenericNode
-
-class FileGroup(GenericNode):
-  pass
--- buildy/nodes/unresolved.py
+++
@@ -1,5 +0,0 @@
-from .generic import GenericNode
-
-class UnresolvedDep(GenericNode):
-  """Node representing an unresolved dependency."""
-  pass
