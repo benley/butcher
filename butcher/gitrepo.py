ec006985d6a6dc0cb2d192116b4fd22badda7121
Benjamin Staffin <ben@cloudscaling.com>
2014-01-13 23:29:57 -0800

    Handle ~ expansion in --map_repo paths
    Change-Id: I36a624a8d0fc2aba42ba814121ff2d79cc928a8b
    CS-Bug: CB-1

--- butcher/gitrepo.py
+++ butcher/gitrepo.py
@@ -51,7 +51,8 @@ class RepoState(app.Module):
     if overrides:
       for line in overrides:
         (reponame, path) = line.split(':')
-        self.origin_overrides[reponame] = os.path.abspath(path)
+        self.origin_overrides[reponame] = os.path.abspath(
+            os.path.expanduser(path))
     pins = app.get_options().pinned_repos
     for pin in (pins or []):
       ppin = address.new(pin)
